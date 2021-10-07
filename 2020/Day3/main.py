e20d1b50fbd201d9b676bf0b74f5ae00187a45a1
VirtualAnirudh <13287843+VirtualAnirudh@users.noreply.github.com>
2021-10-07 13:24:04 +0530

    Finished Day 3

---
+++ 2020/Day3/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+grid = open("input.txt", "r")
+grid = grid.readlines()
+
+row_len = len(grid[0]) - 1
+slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+
+res_1 = []
+res_2 = 1
+
+for (x,y) in slopes:
+    i = 0
+    trees = 0
+
+    for row in grid[::y]:
+        if row[i] == "#":
+            trees = trees + 1
+        i = (i + x) % row_len
+    
+    res_1.append(trees)
+    res_2 = res_2 * trees
+
+print(res_1)
+print(res_2)
