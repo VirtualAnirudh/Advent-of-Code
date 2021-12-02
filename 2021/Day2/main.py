3b019d0d320af40cffe727f3e4dba612387c0afb
VirtualAnirudh <13287843+VirtualAnirudh@users.noreply.github.com>
2021-12-02 11:25:11 +0530

    Finished Day 2

--- 2021/Day2/main.py
+++ 2021/Day2/main.py
@@ -1,1 +1,28 @@
 #!/usr/bin/env python3
+
+commands = [x.strip().split(" ") for x in open("input.txt", "r").readlines()]
+
+x_pos = 0
+y_pos = 0
+
+aim = 0
+depth = 0
+
+for pos,num in commands:
+    num = int(num)
+    if pos == "forward":
+        x_pos += num
+        depth += aim * num
+
+    if pos == "down":
+        y_pos += num
+        aim += num
+    elif pos == "up":
+        y_pos -= num
+        aim -= num
+
+res_1 = x_pos * y_pos
+res_2 = x_pos * depth
+
+print(res_1)
+print(res_2)
