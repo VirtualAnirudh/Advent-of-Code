558d8cc1229f86805f1bbd37695c991d5206a4d3
VirtualAnirudh <13287843+VirtualAnirudh@users.noreply.github.com>
2022-12-02 12:05:30 +0530

    Finished Day 1

--- 2022/Day1/main.py
+++ 2022/Day1/main.py
@@ -1,1 +1,11 @@
-#!/usr/bin/env python3\
+#!/usr/bin/env python3
+
+cals = [x.split("\n") for x in open("input.txt", "r").read().strip().split("\n\n")]
+cals = [sum(int(y) for y in x) for x in cals]
+cals.sort(reverse=True)
+
+res_1 = cals[0]
+res_2 = sum(cals[0:3])
+
+print(res_1)
+print(res_2)
