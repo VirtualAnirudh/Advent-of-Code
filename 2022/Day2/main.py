f831d627f29491e368c21e702ab35394f1212565
VirtualAnirudh <13287843+VirtualAnirudh@users.noreply.github.com>
2022-12-08 21:28:43 +0530

    Finished Day 2

--- 2022/Day2/main.py
+++ 2022/Day2/main.py
@@ -2,11 +2,18 @@
 
 def score(x, y):
     if(x == y):
-        return 3+y
+        return 3 + y
     elif(y == x % 3 + 1):
-        return 6+y
-    return 0+y
+        return 6 + y
+    return y
 
+def score_2(x, y):
+    if(y == 3):
+        return 6 + (x % 3 + 1)
+    elif(y == 2):
+        return 3 + x
+    else:
+        return 0 + ((x + 1) % 3 + 1)
 
 data = open("input.txt", "r")
 data = [x.replace("A", "1")
@@ -18,7 +25,7 @@ data = [x.replace("A", "1")
                     .split(" ") for x in data.read().strip().split("\n")]
 
 res_1 = sum([score(int(x[0]), int(x[1])) for x in data])
-res_2 = 0
+res_2 = sum([score_2(int(x[0]), int(x[1])) for x in data])
 
 print(res_1)
 print(res_2)
