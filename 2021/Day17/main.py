c286f827e1b90ce55fe2fe595bc9a4419dc4560a
VirtualAnirudh <13287843+VirtualAnirudh@users.noreply.github.com>
2022-12-16 02:32:13 +0530

    Removed Unfinished Files

--- 2021/Day17/main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-# Input:
-# target area: x=79..137, y=-176..-117
-
--- 2021/Day4/main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-data = open("input.txt", "r").read().split("\n\n")
-
-draw = data[0].split(",")
-boards = [x.split("\n") for x in data[1:]]
-
